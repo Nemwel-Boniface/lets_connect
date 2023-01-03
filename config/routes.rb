9b84a970dfb1d9096d22a14130ae4d6e80b3e865
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-01-03 08:13:51 +0300

    Update included:   - Updated the users route to use the signup_path named route instead of users_new_url.   - Updated the Users tests to include the signup_path for the tests to pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get 'static_pages/home'
   # get 'static_pages/help'
@@ -9,6 +8,8 @@ Rails.application.routes.draw do
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
+  get '/signup', to:'users#new'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
